1704a53c23da6c44e4c717ac320e13b75ebe4e3e
MaryAnn Chukwuka <annychuks07@gmail.com>
2019-12-18 22:03:45 +0100

    add post model and association

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def create
+    @posts = Post.all
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   attr_accessor :remember_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
