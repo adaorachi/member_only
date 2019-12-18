112b6ae919acf1944d1320dbc393daa70147f5ab
FelipeEnne <felipeenne@gmail.com>
2019-12-18 14:27:15 -0300

    Add the create function on users_controller, add login form and start the session

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
 
   get '/signup', to: 'users#new'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
   resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
